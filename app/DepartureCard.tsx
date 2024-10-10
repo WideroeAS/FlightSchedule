993d4de8f67a7d058b864e51644f6f62af4877e8
Severin Lyngstad <severin.lyngstad@wideroe.no>
2024-10-10 14:08:34 +0200

    chore: use airline logo on airline cards

--- app/DepartureCard.tsx
+++ app/DepartureCard.tsx
@@ -1,19 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
 import { Departure } from '@/app/data/models'
 import style from '@/app/style'
-import { useMemo } from 'react'
+import React, { useMemo } from 'react'
+import { WithLocalSvg } from 'react-native-svg/css'
+import wfLogo from '@/assets/svg/wf.svg'
+import norwegianLogo from '@/assets/svg/norwegian.svg'
+import sasLogo from '@/assets/svg/sas.svg'
 
 export default function DepartureCard(props: { departure: Departure }) {
   const departureTime = props.departure.departureDate.toFormat('HH:mm')
 
-  const airlineColor = useMemo(() => {
+  const airlineIcon = useMemo(() => {
     switch (props.departure.airlineName) {
       case 'Widerøe':
-        return 'green'
+        return wfLogo
       case 'Norwegian':
-        return 'red'
+        return norwegianLogo
       case 'SAS':
-        return 'blue'
+        return sasLogo
     }
   }, [props.departure.airlineName])
 
@@ -25,9 +29,14 @@ export default function DepartureCard(props: { departure: Departure }) {
         <Text>Gate: {props.departure.gate}</Text>
       </View>
 
-      <View style={{ ...cardStyle.secondRow, backgroundColor: airlineColor }}>
+      <View style={{ ...cardStyle.secondRow }}>
         <Text>Flight: {props.departure.flightId}</Text>
         <Text>Airline: {props.departure.airlineName}</Text>
+        <WithLocalSvg
+          asset={airlineIcon as ImageSourcePropType}
+          width={32}
+          height={32}
+        />
       </View>
     </View>
   )
