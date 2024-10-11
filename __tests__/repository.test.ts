47b482866ac2d55e6f44d9bcdb265136cbe7b82a
Severin Lyngstad <severin.lyngstad@wideroe.no>
2024-10-11 13:11:35 +0200

    chore: local departure cache as backup if API goes down

--- __tests__/repository.test.ts
+++ __tests__/repository.test.ts
@@ -1,4 +1,5 @@
 import { useDepartureRepository } from '../app/departure/data/useDepartureRepository'
+import fs from 'fs'
 
 test('Repository fetches departures and converts them to domain model', async () => {
   const repository = useDepartureRepository()
@@ -17,3 +18,22 @@ test('Repository fetches departures and converts them to domain model', async ()
     'departureDate',
   ])
 })
+
+test(
+  'Cache all airport departures',
+  async () => {
+    const repository = useDepartureRepository()
+    const airports = repository.getAirports()
+
+    const data: object = {}
+    for (const airport of airports) {
+      // @ts-ignore
+      data[airport.iata] = await repository.getDepartures(airport.iata)
+      await new Promise(r => setTimeout(r, 1000))
+    }
+
+    const json = JSON.stringify(data)
+    fs.writeFileSync('./app/departure/data/departures.json', json)
+  },
+  2 * 60 * 1000,
+)
