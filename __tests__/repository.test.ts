45ffd590d60c6e24df3b3ebf8aeeec9a567bad84
Severin Lyngstad <severin.lyngstad@wideroe.no>
2024-10-11 15:08:26 +0200

    chore: test both repository types

--- __tests__/repository.test.ts
+++ __tests__/repository.test.ts
@@ -1,19 +1,40 @@
 import useCachedDepartureRepository from '../app/departure/data/useCachedDepartureRepository'
+import { useDepartureRepository } from '../app/departure/data/useDepartureRepository'
 
-test('Repository fetches departures and converts them to domain model', async () => {
-  const repository = useCachedDepartureRepository()
-  const departures = await repository.getDepartures('BOO')
+describe('Repository tests', () => {
+  test('Online repository fetches departures and converts them to domain model', async () => {
+    const repository = useDepartureRepository()
+    const departures = await repository.getDepartures('BOO')
 
-  expect(Array.isArray(departures)).toBe(true)
-  expect(Object.keys(departures[0])).toStrictEqual([
-    'airlineName',
-    'flightId',
-    'fromAirport',
-    'fromAirportName',
-    'toAirport',
-    'toAirportName',
-    'gate',
-    'hasDeparted',
-    'departureDate',
-  ])
+    expect(Array.isArray(departures)).toBe(true)
+    expect(Object.keys(departures[0])).toStrictEqual([
+      'airlineName',
+      'flightId',
+      'fromAirport',
+      'fromAirportName',
+      'toAirport',
+      'toAirportName',
+      'gate',
+      'hasDeparted',
+      'departureDate',
+    ])
+  })
+
+  test('Cached repository fetches departures and converts them to domain model', async () => {
+    const repository = useCachedDepartureRepository()
+    const departures = await repository.getDepartures('BOO')
+
+    expect(Array.isArray(departures)).toBe(true)
+    expect(Object.keys(departures[0])).toStrictEqual([
+      'airlineName',
+      'flightId',
+      'fromAirport',
+      'fromAirportName',
+      'toAirport',
+      'toAirportName',
+      'gate',
+      'hasDeparted',
+      'departureDate',
+    ])
+  })
 })
